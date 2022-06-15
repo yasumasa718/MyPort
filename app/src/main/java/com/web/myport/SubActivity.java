43f90a61224ef2caa981d939f3c0b3572d82deea
yasumasa718 <yasumasa718@gmail.com>
2022-06-15 22:41:17 +0900

    6/15

---
+++ app/src/main/java/com/web/myport/SubActivity.java
@@ -0,0 +1,14 @@
+package com.web.myport;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+    }
+}
